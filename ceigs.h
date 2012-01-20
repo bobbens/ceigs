3c278e73e77515a1f5b939016dd0ae8dacdf1336
Edgar Simo-Serra <bobbens@gmail.com>
2012-01-20 15:11:00 +0100

    * More tweaks.

--- ceigs.h
+++ ceigs.h
@@ -11,7 +11,7 @@
  * @section License
  *
  @verbatim
-    Copyright 2010, 2011 Edgar Simo-Serra
+    Copyright 2011, 2012 Edgar Simo-Serra
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
