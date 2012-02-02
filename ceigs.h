173265f22d68a20a41879407096f9c86918ff996
Edgar Simo-Serra <bobbens@gmail.com>
2012-02-02 11:40:35 +0100

    * Updates.

--- ceigs.h
+++ ceigs.h
@@ -108,6 +108,7 @@
  * - Version 1.2, (unreleased)
  *    - Changed default number of Lanczos vectors to a faster default.
  *    - Tweaked ARPACK parameters to behave like MATLAB.
+ *    - Improved error reporting.
  * - Version 1.1, January 2012
  *    - Invert the eigenvector/value order to match octave/matlab's eigs(...) function.
  *    - Support for EIGS_MODE_I_SHIFTINVERT with default driver backend.
